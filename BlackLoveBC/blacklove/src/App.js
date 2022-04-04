e0fc6bac48df2d43876a25667780a299d0828d95
RLVanessa <vanessaluamba2@icloud.com>
2022-04-04 15:08:53 +0100

    BlackLove

--- BlackLoveBC/blacklove/src/App.js
+++ BlackLoveBC/blacklove/src/App.js
@@ -1,14 +1,11 @@
 import react from "react";
-import Home from "./Pages/Home";
-import Home from "./Pages/AboutUs";
-import Home from "./Pages/New";
-import Home from "./Pages/BestReads";
+
 import './src/index.css';
 
 function App(){
   return (
     <>
-    <Main/>
+    
     </>
   )
 }
