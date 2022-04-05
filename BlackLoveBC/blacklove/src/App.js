bb494a1328c271b1aa1aeb64f131a32502828971
RLVanessa <vanessaluamba2@icloud.com>
2022-04-05 11:58:49 +0100

    added changes to apps.js

--- BlackLoveBC/blacklove/src/App.js
+++ BlackLoveBC/blacklove/src/App.js
@@ -2,6 +2,9 @@ import react from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./Pages/Home";
 import AboutUs from "./Pages/AboutUs";
+import BestReads from "./Pages/BestReads";
+import NewReads from "./Pages/NewReads";
+import MyBooks from "./Pages/MyBooks";
 
 // import './src/index.css';
 
@@ -11,6 +14,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/About" element={<AboutUs />} />
+        <Route path="/Best" element={<BestReads />} />
+        <Route path="/New" element={<NewReads />} />
+        <Route path="/My" element={<MyBooks />} />
+
+
+
       </Routes>
     </BrowserRouter>
   );
